2e5865c94ec447415aa737278bc490be71dac1d5
Willyanne Silva <110118532+maiiyx@users.noreply.github.com>
2023-07-10 18:21:22 -0300

    Add files via upload

---
+++ provaBackEnd/provaBack/index.js
@@ -0,0 +1,75 @@
+// 1°
+//*******************************************
+// const http = require('http')
+// const url = require('url')
+// const address = 'https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf'
+// const parsedURL = new url.URL(address)
+
+// res.setHeader('Content-Type', 'application/json')
+
+// const objeto = [
+//     { id: 1, nome: 'Carlos'},
+//     { id: 2, nome: 'Bruno'},
+//     { id: 3, nome: 'Letícia'},
+// ]
+
+// const json = JSON.stringify(objeto)
+//     console.log(json)
+//*******************************************
+
+
+
+// 2°
+//*******************************************
+//*******************************************
+
+
+
+// 3°
+//*******************************************
+// const url = require('url')
+// const resutUrl = 'https://nodejs.org/api/url.html#url_new_url_input_base'
+// const newUrl = new url.URL(resutUrl)
+
+// const objetos = {
+//     protocol: newUrl.protocol,
+//     slashes: true,
+//     auth: null,
+//     host: newUrl.host,
+//     port: null,
+//     hostname: newUrl.hostname,
+//     hash: newUrl.hash,
+//     search: null,
+//     query: newUrl.query,
+//     pathname: newUrl.pathname,
+//     path: newUrl.path,
+//     href: newUrl.href,
+// }
+//     console.log(objetos)
+//*******************************************
+
+
+
+// 4°
+//*******************************************
+// const fs = require('fs')
+
+// const antigoArq = 'arquivo.txt'
+// const novoArq = 'novo.txt'
+
+// fs.rename(antigoArq, novoArq, (err)=>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log(`Arquivo remoeado de ${antigoArq} para ${novoArq}`)
+// })
+//*******************************************
+
+
+
+// 5°
+//*******************************************
+// const os = require('os')
+//     console.log(os.cpus())
+//*******************************************
